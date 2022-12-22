a09993bd42b0b6b945cbac89502551928006aba1
yeong2222 <tmddud000@naver.com>
2022-12-23 07:38:40 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/ans22/MainActivity.java

---
+++ app/src/main/java/com/example/ans22/elecchapter23.java
@@ -0,0 +1,90 @@
+package com.example.ans22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class elecchapter23 extends AppCompatActivity {
+    private Button next_b23;
+    private Button back_b23;
+    private CheckBox elec_b231;
+    private CheckBox elec_b232;
+    private CheckBox elec_b233;
+    private CheckBox elec_b234;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elecchapter23);
+        next_b23 = findViewById(R.id.next_b23);
+        next_b23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(elecchapter23.this, elecchapter23.class);
+                startActivity(intent);
+            }
+        });
+
+        back_b23 = findViewById(R.id.back_b23);
+        back_b23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(elecchapter23.this,elecchapter22.class);
+                startActivity(intent);
+            }
+        });
+
+
+        elec_b231 = (CheckBox) findViewById(R.id.elec_b231);
+        elec_b232 = (CheckBox) findViewById(R.id.elec_b232);
+        elec_b233 = (CheckBox) findViewById(R.id.elec_b233);
+        elec_b234 = (CheckBox) findViewById(R.id.elec_b234);
+
+
+        elec_b231.setOnClickListener(mCBCL);
+        elec_b232.setOnClickListener(mCBCL);
+        elec_b233.setOnClickListener(mCBCL);
+        elec_b234.setOnClickListener(mCBCL);
+
+    }
+    View.OnClickListener mCBCL = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            boolean checked = ((CheckBox) view).isChecked();
+
+            switch (view.getId()){
+                case R.id.elec_b221:
+
+                    if (checked) {
+                        Toast.makeText(getApplicationContext(), "오답", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+
+                case R.id.elec_b222:
+                    if (checked) {
+                        Toast.makeText(getApplicationContext(), "정답", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+
+                case R.id.elec_b223:
+                    if (checked) {
+                        Toast.makeText(getApplicationContext(), "오답", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case R.id.elec_b224:
+                    if (checked) {
+                        Toast.makeText(getApplicationContext(), "오답", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+
+            }
+
+        }
+
+    };
+}
